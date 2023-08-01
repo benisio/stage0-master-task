4c2028d43c9beac01f3573342fb450546579bfd8
Denis Stepshin <Evil9493@ya.ru>
2023-08-01 17:24:07 +0600

    Complete Lang Syntax stage

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,11 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int sum = 0;
+        String numberStr = String.valueOf(number);
+        for (int i = 0; i < numberStr.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(numberStr.charAt(i)));
+        }
+        System.out.println(sum);
     }
-
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,7 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        String reversed = new StringBuilder(String.valueOf(number)).reverse().toString();
+        System.out.println(Integer.parseInt(reversed));
     }
-
 }
