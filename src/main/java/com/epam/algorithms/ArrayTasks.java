02119db0688f9750cc6829d67bf052faf7b05882
Denis Stepshin <Evil9493@ya.ru>
2023-08-01 19:55:56 +0600

    Complete some tasks

--- src/main/java/com/epam/algorithms/ArrayTasks.java
+++ src/main/java/com/epam/algorithms/ArrayTasks.java
@@ -11,7 +11,7 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-        return null;
+        return new String[]{"Winter", "Spring", "Summer", "Autumn"};
     }
 
     /**
@@ -23,7 +23,11 @@ public class ArrayTasks {
      * length = 1  -> [1] length = 3  -> [1, 2, 3] length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-        return null;
+        int[] resultArr = new int[length];
+        for (int i = 1; i <= length; i++) {
+            resultArr[i] = i;
+        }
+        return resultArr;
     }
 
     /**
@@ -34,7 +38,11 @@ public class ArrayTasks {
      * arr = [1, 3, 5]   -> sum = 9 arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
     }
 
     /**
@@ -46,7 +54,12 @@ public class ArrayTasks {
      * arr = [99, -7, 102], number = -7    ->   2 arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-        return 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == number) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -58,7 +71,11 @@ public class ArrayTasks {
      * "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-        return null;
+        var reversedArr = new String[arr.length];
+        for (int i = 0; i < reversedArr.length; i++) {
+            reversedArr[i] = arr[arr.length - 1 - i];
+        }
+        return reversedArr;
     }
 
     /**
@@ -70,6 +87,15 @@ public class ArrayTasks {
      * arr = [1,-2, 3]      -> [1, 3] arr = [-1, -2, -3]   -> [] arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
+        int positiveNumbersCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                positiveNumbersCount++;
+            }
+        }
+
+        int[] positiveNumbersArr = new int[positiveNumbersCount];
+
         return null;
     }
 
--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -1,8 +1,14 @@
 package com.epam.loops;
 
+import java.util.Scanner;
+
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
+        if (numberTableToPrint > 0) {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(numberTableToPrint + " * " + i + " = " + numberTableToPrint * i);
+            }
+        }
     }
-
 }
