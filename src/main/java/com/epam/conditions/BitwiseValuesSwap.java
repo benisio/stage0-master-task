e5798681514fc7be28d58b5d3529fafa30ef8a52
Denis Stepshin <Evil9493@ya.ru>
2023-08-07 04:14:14 +0600

    Update some tasks

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -3,6 +3,14 @@ package com.epam.conditions;
 public class BitwiseValuesSwap {
 
     public void swap(int first, int second) {
+        first = first ^ second;
+        // binary equivalent of y  will become 0101 ie y=5
+        second = first ^ second;
+        // binary equivalent of x  will become 1010 ie x=10
+        first = first ^ second;
+
+        System.out.println(first);
+        System.out.println(second);
     }
 
 }
